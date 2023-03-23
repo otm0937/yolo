bf266c129da855c93f26d07218f9def156a0ef99
tk_mini <otm0937@naver.com>
2023-03-23 15:02:40 +0900

    add max, min, delay

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ if cap.isOpened():  # check device connection
 prev_time = time.time()  # init time
 cnt = 0
 avg = 1.0
+max_fps = 0
+min_fps = 100000
 
 while True:  # loop
     ret, frame = cap.read()  # get frame
@@ -29,16 +31,25 @@ while True:  # loop
 
         # Count Frame
         now_time = time.time()
-        fps = (1 / (now_time - prev_time))
+        dly = now_time - prev_time
+        fps = (1 / dly)
         avg = (avg * cnt + fps) / (cnt + 1)
         cnt += 1
         prev_time = now_time
 
+        if fps > max_fps:
+            max_fps = fps
+        if fps < min_fps:
+            min_fps = fps
+
         info = "cnt: %d\n fps: %.2f\n avg: %.2f" % (cnt, fps, avg)
 
-        cv2.putText(res_plotted, "cnt: %d" % cnt, (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 0), 1)
-        cv2.putText(res_plotted, "fps: %.2f" % fps, (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 0), 1)
-        cv2.putText(res_plotted, "avg: %.2f" % avg, (5, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 0), 1)
+        cv2.putText(res_plotted, "cnt: %d" % cnt, (5, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 0), 1)
+        cv2.putText(res_plotted, "dly: %.4f" % dly, (5, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 0), 1)
+        cv2.putText(res_plotted, "fps: %.2f" % fps, (5, 70), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 0), 1)
+        cv2.putText(res_plotted, "avg: %.2f" % avg, (5, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 0), 1)
+        cv2.putText(res_plotted, "max: %.2f" % max_fps, (5, 110), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 0), 1)
+        cv2.putText(res_plotted, "min: %.2f" % min_fps, (5, 130), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 255, 0), 1)
 
         #  Display image
         cv2.imshow("result", res_plotted)
